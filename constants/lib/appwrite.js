9ddbe78e55f59d84764efc837d1849f92845a288
kanoksak <atprogrammer@gmail.com>
2024-11-24 07:41:04 +0700

    new

--- constants/lib/appwrite.js
+++ constants/lib/appwrite.js
@@ -5,5 +5,5 @@ export const appwrite = {
     databaseId:'67401d34002fbdc941dd',
     userCollectionId:'67401d87000ea987f29a',
     videoCollectionId:'67401dca0030d5dd8ce6',
-    storageId:'6740204b0018b7ead87fhhh'
+    storageId:'6740204b0018b7ead87fhhhdd'
 }
